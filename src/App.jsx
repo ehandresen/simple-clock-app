7522846da471d18497f22b39c9d13530601721c3
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-27 15:37:17 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header title="CLOCK" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = (props) => {
+  const title = props.title;
+  return <h1 id="clock-title">{title}</h1>;
+};
+
+export default Header;
