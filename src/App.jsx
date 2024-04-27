21c0e8024f19b0bb0052b1b08f02297fa6e12a31
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-27 16:00:15 +0200

    added title and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="container">
-      <Header title="CLOCK" />
+    <div>
+      <div className="header">
+        <Header title="CLOCK" />
+      </div>
+      <div className="container">
+        <Button id="button" variant="primary" onClick={handleClick}>
+          Change Background Color
+        </Button>
+      </div>
     </div>
   );
 };
