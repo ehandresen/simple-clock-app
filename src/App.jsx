fa99c8ff64d27046494e7dcdfcb56f8c96f19f87
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-27 17:19:28 +0200

    moved clock logic to own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import Header from './components/Header';
+import Clock from './components/Clock';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
 const App = () => {
-  const [hour, setHour] = useState(new Date().getHours());
-  const [minutes, setMinutes] = useState(new Date().getMinutes());
-  const [seconds, setSeconds] = useState(new Date().getSeconds());
-
-  function currentTime() {
-    const date = new Date();
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-
-    setHour(hour);
-    setMinutes(minutes);
-    setSeconds(seconds);
-  }
-
-  setInterval(() => {
-    currentTime();
-  }, 1000);
-
   return (
     <div>
       <div className="header">
         <Header title="CLOCK" />
       </div>
       <div className="container">
-        <p id="clock-text">
-          {hour}:{minutes < 10 ? '0' + minutes : minutes}:
-          {seconds < 10 ? '0' + seconds : seconds}
-        </p>
+        <Clock />
       </div>
     </div>
   );
---
+++ src/components/Clock.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const Clock = () => {
+  const [hour, setHour] = useState(new Date().getHours());
+  const [minutes, setMinutes] = useState(new Date().getMinutes());
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  function currentTime() {
+    const date = new Date();
+
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    setHour(hour);
+    setMinutes(minutes);
+    setSeconds(seconds);
+  }
+
+  setInterval(() => {
+    currentTime();
+  }, 1000);
+
+  return (
+    <p id="clock-text">
+      {hour}:{minutes < 10 ? '0' + minutes : minutes}:
+      {seconds < 10 ? '0' + seconds : seconds}
+    </p>
+  );
+};
+
+export default Clock;
